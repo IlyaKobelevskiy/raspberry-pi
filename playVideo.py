dcb949af91b133bba0e7c677ddf36d227200e8e8
IlyaKobelevskiy <ilya.kobelevskiy@gmail.com>
2016-02-22 21:30:07 -0500

    Update playVideo.py

--- playVideo.py
+++ playVideo.py
@@ -1,17 +1,44 @@
 #!/usr/bin/python
- 
+
 # Start xterm in fullscreen mode, execute helper script in console passing it filename as an input
 from subprocess import call
 import sys
-import fcntl 
+import fcntl
+import os
+import re
+import time
+sys.stdout = open("/home/pi/playVideo.out", "a")
 pid_file = 'omxplayer.pid'
 fp = open(pid_file, 'w')
-try:
-	fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
-	# call(["sleep", "1"])
-	# print 'Slept, finishing'
-	call(["xterm", "-fn", "fixed", "-fullscreen", "-e", "omxplayer", sys.argv[1], "-rb"])
-	call(["xrefresh", "-display", ":0"])
-except IOError:
-	# another instance is running
-	print 'Other instance is running, exiting...'
+print "argc = " + str(len(sys.argv))
+for idx, fname in enumerate(sys.argv):
+        print idx, fname
+        if idx == 0:
+                print "Skipping program name"
+                continue
+        loop_condition = True
+        while loop_condition:
+                try:
+                        fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                        srt_file = ''
+                        srt_pattern = re.compile('.srt')
+                        dirname = os.path.dirname(sys.argv[1])
+                        for filename in os.listdir(dirname):
+                                if srt_pattern.search(filename):
+                                        srt_file = dirname + "/" + filename
+                                        break
+                        if not srt_file:
+                                call(["xterm", "-fn", "fixed", "-fg", "black", "-fullscreen", "-e", "omxplayer.sh", fname, "-rb"])
+                                call(["xterm", "-fn", "fixed", "-fullscreen", "-e", "omxplayer", "-r"]);
+                                print 'no subtitle'
+                        else:
+                                call(["xterm", "-fn", "fixed", "-fg", "black", "-fullscreen", "-e", "omxplayer.sh", fname, "--subtitle", srt_file, "-rb"])
+                                call(["xterm", "-fn", "fixed", "-fullscreen", "-e", "omxplayer", "-r"]);
+                                print 'found subtitle:'+srt_file
+                        os.remove('omxplayer.pid')
+                        loop_condition = False
+                except IOError:
+                        # another instance is running
+                        print 'Other instance is running, waiting...'
+                        time.sleep(0.5)
+print "Done"
